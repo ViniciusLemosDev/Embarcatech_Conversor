0c291c3418ad470079640a67214cf00865ac71ae
Kailane Bitencourt <168144028+KailaneBitencourt@users.noreply.github.com>
2024-12-24 13:37:38 -0300

    Alterações

--- Conversor_de_Unidades.c
+++ Conversor_de_Unidades.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <locale.h>
-#include "massa.h"
+#include "massa.c"
 #include "area.h"
 #include "area.c"
-#include "Comprimento.h"
+#include "massa.h"
+#include "comprimento.h"
 
 int main()
 {
@@ -14,27 +15,28 @@ int main()
 
     do
     {
-        printf("Escolha uma das conversões:\n"
+        printf("Escolha uma das convers�es:\n"
                "1 - Unidades de Comprimento\n"
                "2 - Unidades de Massa\n"
                "3 - Unidades de Volume\n"
                "4 - Unidades de Temperatura\n"
                "5 - Unidades de Velocidade\n"
                "6 - Unidades de Energia\n"
-               "7 - Unidades de Área\n"
+               "7 - Unidades de �rea\n"
                "8 - Unidades de Tempo\n"
-               "9 - Unidades de Memória\n"
+               "9 - Unidades de Mem�ria\n"
                "0 - Para sair!\n");
+
         scanf("%d", &tipoUnidade);
 
         switch (tipoUnidade)
         {
         case 1: // Unidades de Comprimento
 
-            printf("Digite a unidade de origem (1. metro, 2. centímetro, 3. milímetro):\n");
+            printf("Digite a unidade de origem (1. metro, 2. cent�metro, 3. mil�metro):\n");
             scanf("%d", &unidadeOrigem);
 
-            printf("Digite a unidade de destino (1. metro, 2. centímetro, 3. milímetro):\n");
+            printf("Digite a unidade de destino (1. metro, 2. cent�metro, 3. mil�metro):\n");
             scanf("%d", &unidadeDestino);
 
             printf("Digite o valor a ser convertido:\n");
@@ -43,26 +45,27 @@ int main()
             resultado = comprimento(unidadeOrigem, unidadeDestino, valor);
 
             if (resultado >= 0)
+            {
                 if ((unidadeOrigem == 1 && unidadeDestino == 2) || (unidadeOrigem == 2 && unidadeDestino == 3))
                 {
-                    printf("O resultado da conversão é: %.3lf\n", resultado);
+                    printf("O resultado da convers�o �: %.3lf\n", resultado);
                 }
                 else
                 {
-                    printf("O resultado da conversão é: %.4lf\n", resultado);
+                    printf("O resultado da convers�o �: %.3lf\n", resultado);
                 }
-
+            }
             printf("Pressione Enter para continuar...\n");
             while (getchar() != '\n')
-                2;     // Limpa buffer
+                ;      // Limpa buffer
             getchar(); // Aguarda Enter
             break;
 
         case 2: // Unidades de Massa
-            printf("\nDigite o número da unidade de origem (1. quilograma, 2. grama, 3. tonelada):\n");
+            printf("\nDigite o n�mero da unidade de origem (1. quilograma, 2. grama, 3. tonelada):\n");
             scanf("%d", &unidadeOrigem);
 
-            printf("Digite o número da unidade de destino (1. quilograma, 2. grama, 3. tonelada):\n");
+            printf("Digite o n�mero da unidade de destino (1. quilograma, 2. grama, 3. tonelada):\n");
             scanf("%d", &unidadeDestino);
 
             printf("Digite o valor a ser convertido:\n");
@@ -71,19 +74,20 @@ int main()
             resultado = massa(unidadeOrigem, unidadeDestino, valor);
 
             if (resultado >= 0)
+            {
                 if ((unidadeOrigem == 1 && unidadeDestino == 3) || (unidadeOrigem == 2 && unidadeDestino == 1))
                 {
-                    printf("O resultado da conversão �: %.4lf\n", resultado);
+                    printf("O resultado da convers�o �: %.4lf\n", resultado);
                 }
                 else if ((unidadeOrigem == 1 && unidadeDestino == 2) || (unidadeOrigem == 3 && unidadeDestino == 1) || (unidadeOrigem == 3 && unidadeDestino == 2))
                 {
-                    printf("O resultado da conversão �: %.2lf\n", resultado);
+                    printf("O resultado da convers�o �: %.2lf\n", resultado);
                 }
                 else
                 {
-                    printf("O resultado da conversão �: %.6lf\n", resultado);
+                    printf("O resultado da convers�o �: %.6lf\n", resultado);
                 }
-
+            }
             printf("Pressione Enter para continuar...\n");
             while (getchar() != '\n')
                 ;      // Limpa buffer
@@ -105,7 +109,7 @@ int main()
             break;
 
         default:
-            printf("Conversão inválida");
+            printf("Convers�o inv�lida");
             printf("Pressione Enter para continuar...\n");
             while (getchar() != '\n')
                 ;      // Limpa buffer
